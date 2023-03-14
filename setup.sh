da0e4d35ae5337b7330539c27bce0680ae914555
Jeffrey Perkel <j_perkel@yahoo.com>
2023-03-14 17:01:08 -0600

    Add user input to setup script.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 # if environments already exist, don't recreate them
 if ! conda env list | grep 'jperkel-' >/dev/null 2>&1; then
+    read -p "Create new environments? [N]: " answer
+    if [[ ! "$answer" == "y" && ! "$answer" == "Y" ]]; then
+        echo "Exiting."
+        exit
+    fi
+
     echo "Creating environments."
     for file in *.yml; do
         conda env create -f "$file"
