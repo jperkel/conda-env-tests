195a6c20697e7ad2998027bcfcd7eac86307792e
Jeffrey Perkel <j_perkel@yahoo.com>
2023-03-14 16:28:21 -0600

    Change 'sex' to 'gender'

--- dataframe.R
+++ dataframe.R
@@ -1,9 +1,9 @@
 # example adapted from 
 # https://swcarpentry.github.io/r-novice-inflammation/12-supp-factors/
-df <- data.frame(id = 1:5, sex = c("F","F","M","F","M"), diet = c("low", "medium", "low", "medium", 
+df <- data.frame(id = 1:5, gender = c("F","F","M","F","M"), diet = c("low", "medium", "low", "medium", 
 "high"))
 df
-print(paste0("Sex is a ", class(df$sex), "."))
-print("Change sex of id=3 ...")
-df$sex[3] <- 'NB'
+print(paste0("Gender is represented as a \'", class(df$sex), "\'."))
+print("Change gender of subject #3 ...")
+df$gender[3] <- 'NB'
 df
