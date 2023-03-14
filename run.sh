010218a13b679db01f3754efbc8ac0d447943be9
C. Titus Brown <titus@idyll.org>
2023-03-14 06:19:58 -0700

    remove build pins; adjust names

--- run.sh
+++ run.sh
@@ -14,12 +14,12 @@ echo "Running hello_world2.py in environment python2"
 conda run -n python2 python hello_world2.py
 echo ""
 echo "Running hello_world2.py in environment python3"
-conda run -n python3 python hello_world2.py
+conda run -n jperkel-py3env python hello_world2.py
 echo ""
 echo "Running dataframe.R in environment r36"
 conda run -n r36 Rscript dataframe.R
 echo ""
 echo "Running dataframe.R in environment r42"
-conda run -n r42 Rscript dataframe.R
+conda run -n jperkel-r42env Rscript dataframe.R
 
 
