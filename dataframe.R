cc09576bd1af02fc9c1270aa387a58a8f79c7bd7
Jeffrey Perkel <j_perkel@yahoo.com>
2023-03-15 10:01:08 -0600

    Fix misnamed colname

--- dataframe.R
+++ dataframe.R
@@ -3,7 +3,7 @@
 df <- data.frame(id = 1:5, gender = c("F","F","M","F","M"), diet = c("low", "medium", "low", "medium", 
 "high"))
 df
-print(paste0("Gender is represented as a \'", class(df$sex), "\'."))
+print(paste0("Gender is represented as a \'", class(df$gender), "\'."))
 print("Change gender of subject #3 ...")
 df$gender[3] <- 'NB'
 df
